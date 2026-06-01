ddfb6b209b74285fa82c6315e0f08693f5c9e5d4
Kai Tait <kaitait@MacBook-Pro.local>
2026-06-01 22:07:36 +1000

    vim warnings

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,6 +17,16 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.dotnet" },
+    { import = "lazyvim.plugins.extras.lang.dart" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "plugins" },
   },
   defaults = {
--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -1,16 +1,4 @@
 return {
-  -- LazyVim language extras for the main stacks I use.
-  { import = "lazyvim.plugins.extras.lang.dotnet" },
-  { import = "lazyvim.plugins.extras.lang.dart" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.terraform" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-
   -- HTML/CSS are mostly covered by LazyVim core + Prettier; this makes their LSPs explicit.
   {
     "neovim/nvim-lspconfig",
