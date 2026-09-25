70d2b737541338530bf04cf04c034255f046cf59
Kai <kaiftait@gmail.com>
2026-09-25 21:00:02 +1000

    Refresh lockfile for contracts v0.5.0

--- .config/sketchybar/plugins/theme_menu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG_DIR="${CONFIG_DIR:-$HOME/.config/sketchybar}"
-
-if [[ -n "$1" ]]; then
-    "$CONFIG_DIR/switch-theme.sh" "$1"
-else
-    sketchybar --set theme_switcher popup.drawing=toggle
-fi
